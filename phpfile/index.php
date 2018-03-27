2fddcdcf90fdc821d8c8d74443cb165c4912e4e0
kurian-thomas <github719@gmai.com>
2018-03-27 15:18:17 +0530

    simple login form usig php

---
+++ phpfile/index.php
@@ -0,0 +1,12 @@
+<?php echo ("<h1>hello world</h1>");?>
+ <html>
+<body>
+
+<form action="welcome.php" method="POST">
+Name: <input type="text" name="name"><br>
+Password: <input type="password" name="pass"><br>
+<input type="submit">
+</form>
+
+</body>
+</html> 
---
+++ phpfile/welcome.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<?php 
+              if($_POST["pass"]=="kurian"){
+			echo("welcome to the sample log in form");
+			echo("<h1>welcome</h1>");
+			echo($_POST["name"]);
+
+
+			}else{
+				echo("Authentication error");
+			} ?><br>
+
+
+</body>
+</html> 
